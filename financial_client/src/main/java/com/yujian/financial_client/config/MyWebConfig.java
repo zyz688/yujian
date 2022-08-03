fe42d8debd4d2aa2dffcc591bc4abf1d162ca31f
zyz688 <1550588295@qq.com>
2022-08-03 00:35:03 +0000

    update financial_client/src/main/java/com/yujian/financial_client/config/MyWebConfig.java.

--- financial_client/src/main/java/com/yujian/financial_client/config/MyWebConfig.java
+++ financial_client/src/main/java/com/yujian/financial_client/config/MyWebConfig.java
@@ -10,14 +10,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @author ze
  * @create 2022-06-26-18:26
  */
+//web 配置
 @Configuration
 public class MyWebConfig implements WebMvcConfigurer {
 
-//    //静态资源释放
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/**")
-//                .addResourceLocations("classpath:/static/");
-//    }
+    //静态资源释放
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+    }
 
 
 
@@ -26,14 +27,23 @@ public class MyWebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 
-       registry.addInterceptor(new LoginInterceptor())
-               .addPathPatterns("/**")
-               .excludePathPatterns("/","/login","/layui/**","/main","addUser");//放行路径
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/**")               //添加拦截路径
+                .excludePathPatterns( //放行路径
+                        "/",
+                        "/login",      //前面两个登入页
+                        "/layui/**",   //静态资源
+                        "/regist",     //注册页面
+                        "/LoginMain",  //登入判断
+                        "/addUser",    //注册用户
+                        "/favicon.ico", //公司图标
+                        "/img/**",      //放行图片
+                        "/captcha",     //验证码
+                        "/loginOut",     //退出请求
+                        "/dist/**"        //自定义插件
+                         );
+
+
     }
-
-
-
-
-
 
 }
